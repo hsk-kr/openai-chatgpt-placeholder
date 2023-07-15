f9d47894e1a01948482a9996a18ee788e231c39c
hsk-kr <hsk.coder@gmail.com>
2023-07-15 23:05:48 +0200

    chore: remove unnecessary types

--- src/types/chrome.d.ts
+++ src/types/chrome.d.ts
@@ -3,36 +3,6 @@
 
 namespace chrome {
   namespace custom {
-    type ChromeMessageType = 'GET_CHAT_TITLES' | 'SET_PLACEHOLDER';
-
-    interface ChromeResponse {
-      type: ChromeMessageType;
-    }
-
-    interface ChromeGetChatTitlesResponse extends ChromeResponse {
-      type: 'GET_CHAT_TITLES';
-      titles: string[];
-    }
-
-    interface ChromeMessageTemplate {
-      type: ChromeMessageType;
-    }
-
-    interface ChromeGetChatTitlesMesasge extends ChromeMessageTemplate {
-      type: 'GET_CHAT_TITLES';
-    }
-
-    interface ChromeSetPlaceholderMessage extends ChromeMessageTemplate {
-      type: 'SET_PLACEHOLDER';
-      placeholder: string;
-    }
-
-    type ChromeMessageResponse = ChromeGetChatTitlesResponse;
-
-    type ChromeMessage =
-      | ChromeGetChatTitlesMesasge
-      | ChromeSetPlaceholderMessage;
-
     type PlaceholderListItem = {
       id: string;
       title: string;
